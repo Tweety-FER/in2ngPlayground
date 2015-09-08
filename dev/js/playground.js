945ecb9a855177e0e3fb9884953aec663dfafd47
fkontic <fkontic@gmail.com>
2015-09-08 10:09:47 +0200

    Add unit tests for parseISO

--- dev/js/playground.js
+++ dev/js/playground.js
@@ -29149,6 +29149,66 @@ var minlengthDirective = function() {
   };
   
 })();
+(function() {
+    'use strict';
+
+    angular
+        .module('in2.playground.parseiso.controller', [])
+        .controller('parseISO.controller', parseISOController);
+
+	parseISOController.$inject = ['$scope', 'parseISO'];
+	
+    function parseISOController($scope, parseISO){
+		$scope.parseISO = parseISO;
+		$scope.test = parseISO('2001-02-02T00:00:33-01:00').toUTCString();
+    }
+})();
+(function () {
+    'use strict';
+    angular
+        .module('in2.playground.parseiso.factory', [])
+        .factory('parseISO', ParseISO);
+
+    function ParseISO(){
+        
+        return parseISO;
+
+        function parseISO(dateString) {
+			//regex used for parsing ISO dates
+            var regex = /^(\d{4})(?:-(\d{2})(?:(?:-(\d{2}))(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{1,3}))?)?(?:(?:([Zz])|([+\-\s])(\d{2}):(\d{2})))?)?)?)?$/;
+			
+            var parts = regex.exec(dateString);
+            if (parts == null){  //dateString didnt match our regular expression
+                throw 'Invalid date format, ISO format required.';
+            }
+            
+            var year = parseInt(parts[1]);
+            var month = (parseInt(parts[2]) - 1) || 0;  //0-based month
+            var day = parseInt(parts[3]) || 1;
+            var hour = parseInt(parts[4]) || 0;
+            var minute = parseInt(parts[5]) || 0;
+            var second = parseInt(parts[6]) || 0;
+            var milisecond = parseFloat('0.' + parts[7])*1000 || 0;  //parts[7] reprezents fractions of a second not actual miliseconds
+            var timezoneOffset = 0;
+			var hasZ = parts[8] == 'z' || parts[8] == 'Z';
+            if (!hasZ && parts[9]){
+                var timezoneSign = (parts[9] == '-') ? 1: -1;
+                var timeZoneHour = parseInt(parts[10]);
+                var timeZoneMinute = parseInt(parts[11]);
+                timezoneOffset = timezoneSign*(timeZoneHour*60 + timeZoneMinute);
+            }
+			
+            var date = new Date(Date.UTC(year, month, day, hour, minute, second, milisecond));  //create a date without setting timezone
+            date.setUTCMinutes(date.getUTCMinutes() + timezoneOffset);  //modify date to account for timezone
+            return date;
+        }
+    }
+})();
+(function() {
+    'use strict';
+    angular
+        .module('in2.playground.parseiso', ['in2.playground.parseiso.factory', 'in2.playground.parseiso.controller'])
+})();
 /**
 * @author Luka Skukan
 * @version 0.1.0
@@ -29264,66 +29324,6 @@ var minlengthDirective = function() {
     };
   }
   
-})();
-(function() {
-    'use strict';
-
-    angular
-        .module('in2.playground.parseiso.controller', [])
-        .controller('parseISO.controller', parseISOController);
-
-	parseISOController.$inject = ['$scope', 'parseISO'];
-	
-    function parseISOController($scope, parseISO){
-		$scope.parseISO = parseISO;
-		$scope.test = parseISO('2001-02-02T00:00:33-01:00').toUTCString();
-    }
-})();
-(function () {
-    'use strict';
-    angular
-        .module('in2.playground.parseiso.factory', [])
-        .factory('parseISO', ParseISO);
-
-    function ParseISO(){
-        
-        return parseISO;
-
-        function parseISO(dateString) {
-			//regex used for parsing ISO dates
-            var regex = /^(\d{4})(?:-(\d{2})(?:(?:-(\d{2}))(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{1,3}))?)?(?:(?:([Zz])|([+\-\s])(\d{2}):(\d{2})))?)?)?)?$/;
-			
-            var parts = regex.exec(dateString);
-            if (parts == null){  //dateString didnt match our regular expression
-                throw 'Invalid date format, ISO format required.';
-            }
-            
-            var year = parseInt(parts[1]);
-            var month = (parseInt(parts[2]) - 1) || 0;  //0-based month
-            var day = parseInt(parts[3]) || 1;
-            var hour = parseInt(parts[4]) || 0;
-            var minute = parseInt(parts[5]) || 0;
-            var second = parseInt(parts[6]) || 0;
-            var milisecond = parseFloat('0.' + parts[7])*1000 || 0;  //parts[7] reprezents fractions of a second not actual miliseconds
-            var timezoneOffset = 0;
-			var hasZ = parts[8] == 'z' || parts[8] == 'Z';
-            if (!hasZ && parts[9]){
-                var timezoneSign = (parts[9] == '-') ? 1: -1;
-                var timeZoneHour = parseInt(parts[10]);
-                var timeZoneMinute = parseInt(parts[11]);
-                timezoneOffset = timezoneSign*(timeZoneHour*60 + timeZoneMinute);
-            }
-			
-            var date = new Date(Date.UTC(year, month, day, hour, minute, second, milisecond));  //create a date without setting timezone
-            date.setUTCMinutes(date.getUTCMinutes() + timezoneOffset);  //modify date to account for timezone
-            return date;
-        }
-    }
-})();
-(function() {
-    'use strict';
-    angular
-        .module('in2.playground.parseiso', ['in2.playground.parseiso.factory', 'in2.playground.parseiso.controller'])
 })();
 /**
 * @author Luka Skukan
--- src/parseISO/parseISO.factory.spec.js
+++ src/parseISO/parseISO.factory.spec.js
@@ -8,8 +8,63 @@ describe('parseISO.factory', function () {
         parseISO = _parseISO_;
     }));
 
-    it('', function () {
-		console.log(parseISO('2017'));
-        expect(parseISO('2017')).toEqual(new Date(2017, 0));
+    it('should parse dates as expected', function () {
+		var date = new Date(2017, 0);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+        expect(parseISO('2017')).toEqual(date);
+		
+		date = new Date(2017, 2);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		expect(parseISO('2017-03')).toEqual(date);
+
+		date = new Date(2017, 2, 5);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		expect(parseISO('2017-03-05')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		expect(parseISO('2017-03-05T20:00Z')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		expect(parseISO('2017-03-05T20:00:23Z')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23, 555);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		expect(parseISO('2017-03-05T20:00:23.555Z')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23, 555);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		date.setUTCMinutes(date.getUTCMinutes() - 191);   //UTC+03:11
+		expect(parseISO('2017-03-05T20:00:23.555+03:11')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23, 55);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		date.setUTCMinutes(date.getUTCMinutes() - 191);   //UTC+03:11
+		expect(parseISO('2017-03-05T20:00:23.055+03:11')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23, 555);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		date.setUTCMinutes(date.getUTCMinutes() - 191);   //UTC+03:11
+		expect(parseISO('2017-03-05T20:00:23.555 03:11')).toEqual(date);
+		
+		date = new Date(2017, 2, 5, 20, 00, 23, 555);
+		date.setUTCMinutes(date.getUTCMinutes() - date.getTimezoneOffset());
+		date.setUTCMinutes(date.getUTCMinutes() + 191);   //UTC-03:11
+		expect(parseISO('2017-03-05T20:00:23.555-03:11')).toEqual(date);
+    });
+	
+	it('should throw exceptions because of the wrong date format', function () {
+		expect(function() {parseISO('201')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('20111')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-2')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-5')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05 20:30Z')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20Z')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20:30Z01:00')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20:30Z+01:00')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20:30C01:00')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20:30C')}).toThrow('Invalid date format, ISO format required.');
+		expect(function() {parseISO('2017-02-05T20:30.5555Z')}).toThrow('Invalid date format, ISO format required.');
     });
 });
