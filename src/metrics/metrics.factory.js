91d6d885971345cccc9aa91115cc502abd41ba45
anchyca <ankicagogic@hotmail.com>
2015-10-05 15:30:36 +0200

    Metrics code correction.

--- src/metrics/metrics.factory.js
+++ src/metrics/metrics.factory.js
@@ -4,36 +4,43 @@
         .module('in2.playground.metrics.factory', [])
         .factory('metrics', Metrics);
 		
-	function Metrics() {
+		var kilometersToMilesConst = 0.621371192;
+		var milesToKilometersConst = 1.609344;
+		
+		function Metrics() {
+			
 		
 			var metrics = {
-				kmToMiles : function(milesOrKilometers) {
-					if (typeof milesOrKilometers === 'number') {
-						if (milesOrKilometers < 0 ) {
-							throw 'Negative numbers are not allowed!';
-						} else {
-							return milesOrKilometers * 0.621371192;
-						}
-					} else {
-						throw 'Only numbers are allowed!';
-					}
-				},
-				
-				milesToKm : function(milesOrKilometers) {
-					if (typeof milesOrKilometers === 'number') {
-						if (milesOrKilometers < 0 ) {
-							throw 'Negative numbers are not allowed!';
-						} else {
-							return milesOrKilometers * 1.609344;
-						}
-					} else {
-						throw 'Only numbers are allowed!';
-					}
-				}
+				kmToMiles : kmToMiles,			
+				milesToKm : milesToKm
 			};
 			
 			return metrics;
 		
+		}
+		
+		function kmToMiles(milesOrKilometers) {
+			if (angular.isNumber(milesOrKilometers)) {
+				if (milesOrKilometers < 0 ) {
+					throw 'Negative numbers are not allowed!';
+				} else {
+					return milesOrKilometers * kilometersToMilesConst;
+				}
+			} else {
+				throw 'Only numbers are allowed!';
+			}
+		}
+		
+		function milesToKm (milesOrKilometers) {
+			if (angular.isNumber(milesOrKilometers)) {
+				if (milesOrKilometers < 0 ) {
+					throw 'Negative numbers are not allowed!';
+				} else {
+					return milesOrKilometers * milesToKilometersConst;
+				}
+			} else {
+				throw 'Only numbers are allowed!';
+			}
 		}
 	
 	}
