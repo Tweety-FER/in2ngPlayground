ef08f0cf158be50d3087d2a68c702c59c8bf6af3
stjepank <stjepan.kelemen02@gmail.com>
2015-10-06 10:26:15 +0200

    Changed test of in2Shuffle, mocked only Math.random

--- src/in2Shuffle/in2Shuffle.service.spec.js
+++ src/in2Shuffle/in2Shuffle.service.spec.js
@@ -1,84 +1,47 @@
 describe('in2Shuffle.factory', function(){
-	var testVar, variable, shuffleMock, length, rand, temp;
+	var shuffle, variable, shuffleMock, length, rand, temp, stringVar, object;
 	var randArray= [];
 
 	beforeEach(module('in2.playground.shuffle'));
 
 	beforeEach(inject(function(_shuffle_){
-		//testVar = _shuffle_;
+		shuffle = _shuffle_;
+		var rand = [0.11, 0.83, 0.78, 0.34, 0.54]; //list of values our mocked Math.random will return
+		var i = 0;
+		var returnRand;
+		spyOn(Math, 'random').and.callFake(function(){ //we are mocking random so we can get a not random result
+			returnRand = rand[i];
+			i= i +1;
+			return returnRand;
+		});
 
-		shuffleMock = {
-		shuffle: function(array){
-			var i = 0;
-			length = array.length;
-			for(i=0; i< length; i++){
-				randArray[i]=(Math.floor((length-1)/(i+1)));
-			}
-			if(angular.isArray(array)){
-				shuffleMock.shuffleArray(array, randArray);
-			}
-
-			if(angular.isString(array)){
-				shuffleMock.shuffleString(array, randArray);
-			}
-		},
-		shuffleArray: function(array, randArray){
-			length = array.length;
-			while(length != 0){
-					rand = Math.floor(randArray[length-1]);
-					temp = array[rand];
-					array[rand]= array[length-1];
-					array[length-1]= temp;
-					length= length -1;
-				}
-		},
-		shuffleString: function(array, randArray){
-			length = array.length;
-
-			while(length != 0){
-				rand = Math.floor(randArray[length-1]);
-				array = shuffleMock.switchChars(array, length-1, rand);
-				length= length -1;
-			}
-		},
-		switchChars: function(array, length, rand){
-				temp= array[rand];
-				array = array.substr(0, rand) + array[length] + array.substr(rand+1);
-				array = array.substr(0, length) + temp + array.substr(length+1);
-				return array;
-			} 
-	}
-		spyOn(shuffleMock, 'shuffleArray').and.callThrough();
-		spyOn(shuffleMock, 'shuffleString').and.callThrough();
 		variable = [1, 2, 3, 4, 5];
-		shuffleMock.shuffle(variable);
+		variable = shuffle(variable);
+
+		i=0;
+		stringVar = '1es2n';
+		stringVar = shuffle(stringVar);
+
+		i=0;
+		object = {test: 'rand',
+		test2: 'rand2'};
+		object = shuffle(object);
 	}));
 	
-
 	
 	it('should be defined', function(){
-		expect(shuffleMock).toBeDefined();
+		expect(shuffle).toBeDefined();
 	});
 
-	it('should shuffle array [1,2,3,4,5] and return [1,4,3,2,5]', function(){
-		expect(variable).toEqual([1,4,3,2,5]);
+	it('should shuffle array [1,2,3,4,5] and return [4,5,2,3,1]', function(){
+		expect(variable).toEqual([4,5,2,3,1]);
 	});
 
-	it('should call shuffleArray if variable is array', function(){
-		//shuffleMock.shuffle(variable);
-		expect(shuffleMock.shuffleArray).toHaveBeenCalled();
-	});
-
-	it('should call shuffleString if variable is string', function(){
-		var string = 'testingShuffle';
-		shuffleMock.shuffle(string);
-		expect(shuffleMock.shuffleString).toHaveBeenCalled();
+	it('should shuffle string "1es2n" and return "2nes1"', function(){
+		expect(stringVar).toEqual('2nes1');
 	});
 
 	it('should return the same variable as sent if it is not an array or a string', function(){
-		var object = {test: 'rand',
-		test2: 'rand2'};
-		shuffleMock.shuffle(object);
 		expect(object).toEqual(object);
 	});
 
